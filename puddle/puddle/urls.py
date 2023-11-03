7fb7b8ae478a0cae64936a3881b7f9725b3892b8
Michael Oguidan <michaeloguidan@gmail.com>
2023-11-03 06:56:45 +0000

    Add item detail view and update index template

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core.views import index, contact
+from item.views import detail
 
 urlpatterns = [
     path('', index, name='index'),
+    path('item/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
