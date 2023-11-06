07e0f4ff25d9928d9c0da757b9a9784259d6785e
Michael Oguidan <michaeloguidan@gmail.com>
2023-11-06 04:05:21 +0000

    Added dashboard app and updated base template

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -8,5 +8,6 @@ from core.views import index, contact, signup
 urlpatterns = [
     path('', include('core.urls')),
     path('item/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
