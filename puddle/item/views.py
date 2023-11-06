cbd96a909e3176e382204148f537a75f482de527
Michael Oguidan <michaeloguidan@gmail.com>
2023-11-06 06:40:40 +0000

    Add browse items link to base template and create items view

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
-from .models import Item
+from .models import Item, Category
 from .forms import NewItemForm, EditItemForm
 
 # Create your views here.
+def items(request):
+    category_id = request.GET.get('category', 0)
+    query = request.GET.get('query', '')
+    items = Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'item/items.html', {
+        'items':items,
+        'query':query,
+        'categories': categories,
+        'category_id':int(category_id)
+    })
+
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category = item.category, is_sold = False).exclude(pk = pk)[0:3]
